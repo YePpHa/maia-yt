4c6f2634ee9e4ad43d8b50a83567d717dad74dab
Jeppe Rune Mortensen <jepperm@gmail.com>
2017-05-04 19:42:18 +0200

    Updated google-closure-compiler and google-closure-library. Added closure test.

--- bin/build-gulp.js
+++ bin/build-gulp.js
@@ -4,15 +4,20 @@ var gulp = require('gulp')
   , closureCompiler = require('google-closure-compiler').gulp()
   , sourcemaps = require('gulp-sourcemaps');
 
-exports.closureCompiler = function(entryPoint, output, externs, defines, output_wrapper) {
+exports.closureCompiler = function(options) {
+  if (!options.entry_point) throw new Error("No entry point declared.");
+  if (!options.checks_only && !options.output_file)
+    throw new Error("No output file declared.");
+
   var src = path.join(__dirname, '..', 'src', 'js', '**', '*.js');
-  var dist = path.dirname(output);
+  var dist = options.output_file && path.dirname(options.output_file);
 
   var libs = [];
   libs.push(path.join(__dirname, '..', 'node_modules', 'google-closure-library', '**.js'));
+  libs.push("!" + path.join(__dirname, '..', 'node_modules', 'google-closure-library', '**_test.js'));
 
-  externs = externs || [];
-  defines = defines || {};
+  var externs = options.externs || [];
+  var defines = options.defines || {};
 
   var definesC = [];
   Object.keys(defines).forEach(function(key) {
@@ -20,25 +25,50 @@ exports.closureCompiler = function(entryPoint, output, externs, defines, output_
     definesC.push(key + "=" + value);
   });
 
-  output_wrapper = output_wrapper || '(function(){\n%output%\n}).call(this)';
+  var output_wrapper = options.output_wrapper || '(function(){\n%output%\n}).call(this)';
 
-  return gulp.src(src, { base: '../' })
-    //.pipe(sourcemaps.init())
-    .pipe(closureCompiler({
-        js: libs,
-        define: definesC,
-        compilation_level: 'ADVANCED',
-        warning_level: 'VERBOSE',
-        dependency_mode: 'STRICT',
-        entry_point: entryPoint,
-        language_in: 'ECMASCRIPT6_STRICT',
-        language_out: 'ECMASCRIPT5_STRICT',
-        output_wrapper: output_wrapper,
-        js_output_file: path.basename(output),
-        env: 'BROWSER',
-        externs: externs,
-        assume_function_wrapper: true
-      }))
-    //.pipe(sourcemaps.write('/'))
-    .pipe(gulp.dest(dist));
+  var os = gulp.src(src, { base: '.' });
+  if (options.sourcemaps) {
+    os = os.pipe(sourcemaps.init());
+  }
+
+  var closure_options = {
+    js: libs,
+    define: definesC,
+    compilation_level: 'ADVANCED',
+    warning_level: 'VERBOSE',
+    dependency_mode: 'STRICT',
+    entry_point: options.entry_point,
+    language_in: 'ECMASCRIPT6_STRICT',
+    language_out: 'ECMASCRIPT5_STRICT',
+    output_wrapper: output_wrapper,
+    externs: externs,
+    assume_function_wrapper: true
+  };
+  if (options.checks_only) {
+    closure_options.checks_only = true;
+    closure_options.jscomp_error = [
+      "checkTypes",
+      "conformanceViolations",
+      "functionParams",
+      "globalThis",
+      "invalidCasts",
+      "misplacedTypeAnnotation",
+      "nonStandardJsDocs",
+      "suspiciousCode",
+      "uselessCode"
+    ];
+  } else {
+    closure_options.js_output_file = path.basename(options.output_file);
+  }
+
+  os = os.pipe(closureCompiler(closure_options));
+
+  if (options.sourcemaps) {
+    os = os.pipe(sourcemaps.write('/'));
+  }
+
+  if (options.checks_only)
+    return os;
+  return os.pipe(gulp.dest(dist));
 };
--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,47 @@ var gulp = require('gulp')
 
 var injectFile = path.join(__dirname, 'tmp', 'maia.inject.js');
 
-gulp.task('build-inject', function() {
-  return closureGulp('/src/js/inject/index', injectFile);
+gulp.task('build:inject', function() {
+  return closureGulp({
+    entry_point: '/src/js/inject/index',
+    output_file: injectFile
+  });
 });
 
-gulp.task('build-userscript', ['build-inject'], function() {
+gulp.task('build:userscript', ['build:inject'], function() {
   var metadataBlock = fs.readFileSync(path.join(__dirname, 'src', 'userscript.metablock'));
 
   var dist = path.join(__dirname, 'dist', 'maia.user.js');
 
-  return closureGulp('/src/js/userscript/index', dist, [
-    path.join(__dirname, 'src', 'externs', 'greasemonkey.js')
-  ], {
-    CORE_INJECT_JS: fs.readFileSync(injectFile)
-  }, metadataBlock + '(function(){\n%output%\n}).call(this)');
+  return closureGulp({
+    entry_point: '/src/js/userscript/index',
+    output_file: dist,
+    externs: [
+      path.join(__dirname, 'src', 'externs', 'greasemonkey.js')
+    ],
+    defines: {
+      CORE_INJECT_JS: fs.readFileSync(injectFile)
+    },
+    output_wrapper: metadataBlock + '(function(){\n%output%\n}).call(this)'
+  });
+});
+
+gulp.task('test:closure', ['test:closure:inject', 'test:closure:userscript']);
+gulp.task('test:closure:inject', function() {
+  return closureGulp({
+    entry_point: '/src/js/inject/index',
+    checks_only: true
+  });
+});
+gulp.task('test:closure:userscript', function() {
+  return closureGulp({
+    entry_point: '/src/js/userscript/index',
+    checks_only: true,
+    externs: [
+      path.join(__dirname, 'src', 'externs', 'greasemonkey.js')
+    ],
+    defines: {
+      CORE_INJECT_JS: "/*NOT NEEDED FOR THE TEST*/"
+    }
+  });
 });
